d1c74c7611c4f23f3c4343caec3513677b8b2942
galmanzur <gal1942000@gmail.com>
2020-04-20 15:27:54 +0300

    changed post form

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('author', 'title', 'text',)
 
 
 class CommentForm(forms.ModelForm):
